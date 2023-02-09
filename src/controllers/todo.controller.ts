f9c4e25242ff1703f3ec65e014764ff79d383f9f
Felipe <antoniofelipesilva@live.com>
2023-02-09 19:17:48 -0300

    version.1.0.0

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -2,10 +2,19 @@ import { Request, Response } from 'express';
 import { Todo } from '../models/Todo';
 
 export const all = async (req: Request, res: Response ) => {
-    res.send('teste')
+    const list = await Todo.findAll()
+    res.json({ list })
 }
 export const add = async (req: Request, res: Response ) => {
-    res.send('add')
+    if(req.body.title){
+        let newTodo = await Todo.create({
+            title: req.body.title,
+            done: req.body.done ? true : false
+        })
+        res.status(201).json({ newTodo })
+    }else{
+        res.json({ error: 'Dados não enviados!'})
+    }
 }
 export const update = async (req: Request, res: Response ) => {
     res.send('update')
